b6e35e9b31cf4d0d3803aca290927e0a9e1af888
NamazG <96447045+NamazG@users.noreply.github.com>
2022-01-10 21:31:22 -0700

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult NewsDetail()
+        {
+            return View();
+        }
+    }
+}
